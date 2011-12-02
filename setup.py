fa7732b93b3ca3735c3509f47a5d2782734bf279
Jonas Baumann <jone@jone.ch>
2011-12-02 13:38:40 +0100

    Fix setup.py to use README.rst, not .txt

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ mainainter = 'Philipp Gross'
 setup(name='ftw.dashboard.portlets.favourites',
       version=version,
       description="A favourite Portlet, which shows your favourites on the dashboard",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
